ea78a4ce6111eb36e57eb918e74c8a5f1375b1f4
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2015-05-09 17:09:38 -0700

    Add project files

---
+++ FullCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FullCount
+//
+//  Created by Jean-Baptiste Louazel on 09/05/2015.
+//  Copyright (c) 2015 Jean-Baptiste Louazel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
