e7e019a75b3729dbff6f767902a35cffc7101ea3
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2015-05-12 04:20:18 -0700

    Finish the user card page

--- FullCount/PlayerCardViewController.swift
+++ FullCount/PlayerCardViewController.swift
@@ -11,7 +11,13 @@ import UIKit
 class PlayerCardViewController: UIViewController {
 	@IBOutlet weak var menuButton: UIBarButtonItem!
 	
+	@IBOutlet weak var userNameLabel: UILabel!
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var teamNameLabel: UILabel!
 	
+	var user: User!
+	let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -20,6 +26,25 @@ class PlayerCardViewController: UIViewController {
 			menuButton.action = "revealToggle:"
 			self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
 		}
+		
+		var data = self.prefs.dataForKey("user")!
+		user = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? User
+		
+		let usDateFormat = NSDateFormatter.dateFormatFromTemplate("MMddyyyy", options: 0, locale: NSLocale(localeIdentifier: "en-US"))
+
+		var dateFormatter = NSDateFormatter()
+		dateFormatter.dateFormat = "MMMM d, YYYY"
+		let d = NSDate()
+		
+		userNameLabel.text = user.username?.uppercaseString
+		teamNameLabel.text = user.team?.name?.uppercaseString
+		
+		dateLabel.text = dateFormatter.stringFromDate(d).uppercaseString
+		dateLabel.text?.uppercaseString
+		
+		userNameLabel.transform = CGAffineTransformMakeRotation (3.14/0.518)
+		dateLabel.transform = CGAffineTransformMakeRotation (3.14/0.518)
+		teamNameLabel.transform = CGAffineTransformMakeRotation (3.14/0.52)
 	}
 	
 	override func didReceiveMemoryWarning() {
